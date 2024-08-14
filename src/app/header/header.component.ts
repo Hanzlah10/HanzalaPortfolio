a34dc31a4511575d506555796afcc6662f14688e
Hanzala Sarguroh <hanzalasarguroh@gmail.com>
2024-08-15 02:41:39 +0530

    chore: Optimize header and hero components with lazy loading for images

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,21 +13,9 @@ export class HeaderComponent implements OnInit {
   constructor(private projectService: ProjectsService) { }
   isDarkMode = false;
   views$!: Observable<any>;
-  // views!: any;
   ngOnInit() {
     this.views$ = this.projectService.getViews()
-    // this.projectService.getViews().subscribe((data) => {
-    //   console.log(data);
-    // });
-    console.log('running');
-
-    console.log(this.views$);
-
   }
-
-  // ngOnInit(): void {
-
-  // }
 
   toggleDarkMode() {
     this.isDarkMode = !this.isDarkMode;
